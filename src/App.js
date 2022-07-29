d1c7f0433f36c5b17260638d42ed21c9b869264d
princeak <akalodave@gmail.com>
2022-07-29 14:32:43 +0100

    latest changes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
@@ -14,58 +16,66 @@ import Footer from "./components/Footer";
 import FullBlogPost from "./components/FullPost";
 
 function App() {
-  const BlogPostSection = styled.div`
-    display: flex;
-  `;
 
-  const Blog = styled.div`
-    flex: 2;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  `;
+    const [title, setTitle] = useState("Default Title");
 
-  const About = styled.div`
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    align-items: space-between;
-    // background-color: red;
-  `;
+    useEffect(() => {
+      // This will run when the page first loads and whenever the title changes
+      document.title = title;
+    }, [title]);
+    
+    const BlogPostSection = styled.div`
+      display: flex;
+    `;
 
-  const SearchBox = styled.div`
-    display: flex;
-    align-items: center;
-    width: 70%;
-    height: 50px;
-    margin-top: 60px;
-    // background-color: red;
-  `;
+    const Blog = styled.div`
+      flex: 2;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    `;
 
-  return (
-    <>
-      <Header />
-      <BlogTitle />
-      <ImageSlider />
-      <BlogPostSection>
-        <Blog>
-          <BlogPost />
-          <BlogPost />
-          <BlogPost />
-        </Blog>
-        <About>
-          <AuthorInfo />
-          <FollowSocials />
-          <RecentPosts />
-          <SearchBox>
-            <Search />
-          </SearchBox>
-          <Categories />
-        </About>
-      </BlogPostSection>
-      <Footer />
-    </>
-  );
+    const About = styled.div`
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      align-items: space-between;
+      // background-color: red;
+    `;
+
+    const SearchBox = styled.div`
+      display: flex;
+      align-items: center;
+      width: 70%;
+      height: 50px;
+      margin-top: 60px;
+      // background-color: red;
+    `;
+
+    return (
+      <>
+        <Header />
+        <BlogTitle />
+        <ImageSlider />
+        <BlogPostSection>
+          <Blog>
+            <BlogPost />
+            <BlogPost />
+            <BlogPost />
+          </Blog>
+          <About>
+            <AuthorInfo />
+            <FollowSocials />
+            <RecentPosts />
+            <SearchBox>
+              <Search />
+            </SearchBox>
+            <Categories />
+          </About>
+        </BlogPostSection>
+        <Footer />
+      </>
+    );
 }
 
 export default App;
